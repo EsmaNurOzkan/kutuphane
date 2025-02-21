073ffe1d7121b3ff93be153014bf0811ed0f5dde
EsmaNurOzkan <nuresmaozkan98@gmail.com>
2025-02-21 14:38:54 +0300

    login updated

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -5,7 +5,7 @@ import { Form, Button, Card, Container, Row, Col } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
 import "../../style/App.css"
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL; 
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -17,7 +17,7 @@ function Login() {
     try {
       const response = await axios.post(`${BACKEND_URL}/api/auth/login`, { email, password });
       localStorage.setItem('token', response.data.token);
-      navigate('/home');
+      navigate('/');
       window.location.reload();
     } catch (error) {
       console.log("Backend URL:", process.env.REACT_APP_BACKEND_URL);
@@ -30,58 +30,58 @@ function Login() {
 
   return (
     <>
-    <Helmet>
+      <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&display=swap" rel="stylesheet" />
 
-        <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet" />
 
-    </Helmet>
-    <Container className="d-flex align-items-center justify-content-center " style={{ minHeight: '100vh' }}>
-      <Row className="w-100">
-        <Col sm={12} md={8} lg={6} className="mx-auto">
-        <Card className=" border-0 shadow-lg bg-body" style={{ borderRadius: '3rem'  }}>
-          <Card.Body>
-              <h3 className="text-center mb-4" style={{ fontFamily: 'DM Serif Text, serif' }}>Giriş Yap</h3>
-              <Form onSubmit={handleSubmit}>
-                <Form.Group id="email">
-                  <Form.Label style={{ fontFamily: "Cormorant, serif", fontWeight : 600 }}>E-posta</Form.Label>
-                  <Form.Control
-                    style={{ fontFamily: "Cormorant, serif", fontWeight : 400 }}
-                    className="fs-5"
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="E-posta"
-                    required
-                  />
-                </Form.Group>
-                <Form.Group id="password" className="mt-3">
-                  <Form.Label style={{ fontFamily: "Cormorant, serif", fontWeight : 600 }}>Şifre</Form.Label>
-                  <Form.Control
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Şifre"
-                    required
-                  />
-                </Form.Group>
-                <Button style={{ fontFamily: "Cormorant, serif", fontWeight : 500 }} type="submit" className=" w-100 mt-4">
-                  Giriş Yap
-                </Button>
-              </Form>
-              <Link to="/reset-password" style={{ fontFamily: "Cormorant, serif", fontWeight : 500 }} className="w-100 mt-3 d-block text-center">
-                <a className="link-primary">Şifremi unuttum</a>
-              </Link>
-              <Link to="/register" style={{ fontFamily: "Cormorant, serif", fontWeight : 500 }} className=" w-100 mt-1 d-block text-center ">
-                <a className='link-secondary' >Kayıt Ol</a>
-              </Link>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+      </Helmet>
+      <Container className="d-flex align-items-center justify-content-center " style={{ minHeight: '100vh' }}>
+        <Row className="w-100">
+          <Col sm={12} md={8} lg={6} className="mx-auto">
+            <Card className=" border-0 shadow-lg bg-body" style={{ borderRadius: '3rem' }}>
+              <Card.Body>
+                <h3 className="text-center mb-4" style={{ fontFamily: 'DM Serif Text, serif' }}>Giriş Yap</h3>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group id="email">
+                    <Form.Label style={{ fontFamily: "Cormorant, serif", fontWeight: 600 }}>E-posta</Form.Label>
+                    <Form.Control
+                      style={{ fontFamily: "Cormorant, serif", fontWeight: 400 }}
+                      className="fs-5"
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="E-posta"
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group id="password" className="mt-3">
+                    <Form.Label style={{ fontFamily: "Cormorant, serif", fontWeight: 600 }}>Şifre</Form.Label>
+                    <Form.Control
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Şifre"
+                      required
+                    />
+                  </Form.Group>
+                  <Button style={{ fontFamily: "Cormorant, serif", fontWeight: 500 }} type="submit" className=" w-100 mt-4">
+                    Giriş Yap
+                  </Button>
+                </Form>
+                <Link to="/reset-password" style={{ fontFamily: "Cormorant, serif", fontWeight: 500 }} className="w-100 mt-3 d-block text-center">
+                  <a className="link-primary">Şifremi unuttum</a>
+                </Link>
+                <Link to="/register" style={{ fontFamily: "Cormorant, serif", fontWeight: 500 }} className=" w-100 mt-1 d-block text-center ">
+                  <a className='link-secondary' >Kayıt Ol</a>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
